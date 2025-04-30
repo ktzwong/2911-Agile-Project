19d3d0131957b2c24ebb62d2d173fc7c13d3cbc3
Kevin Wong <twong358@my.bcit.ca>
2025-04-30 11:13:07 -0700

    flask setup and file organization

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for
+from pathlib import Path
+
+app = Flask(__name__)
+app.instance_path = Path(" ").resolve()
+
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+
+if __name__ == "__main__":
+     app.run(debug=True, port=8888)
